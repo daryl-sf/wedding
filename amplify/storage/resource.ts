308899c4a75b7d94e0c069d94b6e1a2ecd8954df
Daryl Findlay <darylfindlay@gmail.com>
2025-05-31 23:24:50 +0100

    permissions

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -2,4 +2,10 @@ import { defineStorage } from '@aws-amplify/backend';
 
 export const storage = defineStorage({
   name: 'wedding-pictures',
+  access: (allow) => ({
+    "photos/*": [
+      allow.guest.to(["read"]),
+      allow.authenticated.to(["write", "delete", "delete", "read"])
+    ],
+  })
 });
