cb2db3fd81d7095b0c88debf3b53e60a8c24c480
Daryl Findlay <darylfindlay@gmail.com>
2025-06-01 22:17:49 +0100

    image grid

--- amplify/storage/resource.ts
+++ amplify/storage/resource.ts
@@ -4,8 +4,7 @@ export const storage = defineStorage({
   name: 'wedding-pictures',
   access: (allow) => ({
     "photos/*": [
-      allow.guest.to(["read"]),
-      allow.authenticated.to(["write", "delete", "delete", "read"])
-    ],
-  })
+      allow.guest.to(["list", "get", "write"]),
+    ]
+  }),
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,143 @@
 import { useEffect, useState } from "react";
-import { useAuthenticator } from "@aws-amplify/ui-react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
-import FileUploader from "./FileUploader";
+import { getUrl, list, uploadData } from "aws-amplify/storage";
+import { CameraIcon } from "@heroicons/react/24/outline";
 
-const client = generateClient<Schema>();
+type ImageItem = {
+  url: string;
+  versionId?: string | undefined;
+  contentType?: string | undefined;
+  lastModified?: Date | undefined;
+  size?: number | undefined;
+  eTag?: string | undefined;
+  path: string;
+};
 
 function App() {
-  const { user, signOut } = useAuthenticator();
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  function deleteTodo(id: string) {
-    client.models.Todo.delete({ id });
-  }
+  const [, setLoading] = useState(true);
+  const [images, setImages] = useState<ImageItem[]>([]);
 
   useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
+    loadImages();
   }, []);
 
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
+  const loadImages = async () => {
+    setLoading(true);
+    try {
+      const result = await list({
+        path: "photos/",
+      });
+
+      const imagePromises = result.items.map(async (item) => {
+        try {
+          const url = await getUrl({
+            path: item.path,
+          });
+          return {
+            ...item,
+            url: url.url.toString(),
+          };
+        } catch (error) {
+          console.error("Error getting URL for", item.path, error);
+          return null;
+        }
+      });
+
+      const imageUrls = await Promise.all(imagePromises);
+      const validImages = imageUrls.filter((img) => img !== null);
+
+      // Sort by newest first
+      // validImages.sort((a, b) => new Date(b.lastModified) - new Date(a.lastModified));
+
+      setImages(validImages);
+    } catch (error) {
+      console.error("Error loading images:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const fileArray = Array.from(files);
+      fileArray.map((file) => {
+        upload(file);
+      });
+      event.target.value = "";
+    }
+  };
+
+  const upload = async (file: File) => {
+    if (!file) {
+      return;
+    }
+    const result = await uploadData({
+      path: `photos/${file.name}`,
+      data: file,
+    }).result;
+
+    const url = await getUrl({
+      path: result.path,
+    });
+    const image = {
+      ...result,
+      url: url.url.toString(),
+    };
+    setImages([image, ...images]);
+  };
 
   return (
     <main>
-      <h1>{user?.signInDetails?.loginId}'s todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() => deleteTodo(todo.id)}>
-            {todo.content}
-          </li>
+      <label
+        htmlFor="file-upload"
+        style={{
+          width: "4rem",
+          height: "4rem",
+          borderRadius: "50%",
+          backgroundColor: "purple",
+          color: "white",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "fixed",
+          bottom: "2rem",
+          right: "2rem",
+        }}
+      >
+        <CameraIcon style={{ height: "2rem", width: "2rem" }} />
+        <input
+          id="file-upload"
+          type="file"
+          accept="image/*"
+          multiple
+          onChange={handleChange}
+          style={{ display: "none" }}
+        />
+      </label>
+      <div
+        style={{
+          width: "100vw",
+          height: "100dvh",
+          display: "flex",
+          flexWrap: "wrap",
+          flexDirection: "row",
+        }}
+      >
+        {images.map((image) => (
+          <img
+            key={image.path}
+            src={image.url}
+            alt={image.path}
+            style={{
+              width: "100%",
+              height: "auto",
+              // maxWidth: "400px",
+              maxHeight: "400px",
+              objectFit: "cover",
+            }}
+          />
         ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
       </div>
-      <FileUploader />
-      <br />
-      <br />
-
-      <button onClick={signOut}>Sign out</button>
     </main>
   );
 }
